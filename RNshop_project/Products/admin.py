4f2bf55ee84af068c71dca5cc75e8257981f078c
R1m4a <rima.neroo@gmail.com>
2025-09-03 17:48:42 +0200

    made second model for categories, connected categories by foreign key and small changes in models.

--- RNshop_project/Products/admin.py
+++ RNshop_project/Products/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug':('name',)}
+
+
+
 
 class ProductAdmin(admin.ModelAdmin):
+    
     list_display = ('name', 'price', 'added_on','produced_by','availability')
+
     list_editable = ('availability', 'price')
+    
     list_filter = ('availability','produced_by')
 
+    prepopulated_fields = {'slug':('name',)}
+
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 
--- RNshop_project/Products/models.py
+++ RNshop_project/Products/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    slug = models.SlugField(unique = True)
+    
+    
+    def __str__(self):
+        return self.name
+
+
+
 class Product(models.Model):
     Brand_choices=[("apple","Apple"),
                   ("xiaomi,", "Xiaomi"),
@@ -8,17 +19,22 @@ class Product(models.Model):
                   ("nokia","Nokia")]
     
 
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
 
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length = 60)
 
-    price = models.DecimalField(max_digits=7, decimal_places= 2)
+    slug = models.SlugField(unique = True)
+
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
 
     photo_presentation = models.ImageField(upload_to='products/Media')
 
     added_on = models.DateField(auto_now_add = True)
 
-    description = models.TextField(blank=True)
+    description = models.TextField(blank = True)
 
     produced_by = models.CharField(max_length = 20 ,choices = Brand_choices)
 
-    availability = models.BooleanField(default=True)
+    availability = models.BooleanField(default = True)
+
+
